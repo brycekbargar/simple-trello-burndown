8b03442ce12c3cc02ed6bdbb0a97f81bedb8379f
bryce <brycekbargar@gmail.com>
2016-08-23 11:42:25 +0000

    Get CardHistories with statuses

--- api/model/cardHistory.js
+++ api/model/cardHistory.js
@@ -2,23 +2,25 @@
 const knexFactory = require('./knexFactory.js');
 
 function CardHistory(data) {
-  this.cardNo = data.cardNo;
-  this.listId = data.listId;
+  if(data.cardNo) this.card_no = data.cardNo;
+  if(data.listId) this.list_id = data.listId;
+  if(data.created_at) this.createdAt = data.created_at;
+  if(data.status) this.status = data.status;
 }
 
 CardHistory.bulkCreate = cardHistories =>
   knexFactory().transaction(tx => 
-    Promise.all(
-      cardHistories
-        .map(ch => tx
-          .insert({
-            card_no: ch.cardNo,
-            list_id: ch.listId
-          })
-          .into('card_histories'))));
+    Promise.all(cardHistories.map(ch =>
+      tx.insert(ch).into('card_histories'))));
 
-CardHistory.list = function() {
-  return [];
-};
+CardHistory.list = () =>
+  knexFactory().select([
+    'CH.created_at',
+    'L.status'
+  ])
+  .from('card_histories as CH')
+  .innerJoin('lists as L', 'CH.list_id', 'L.id')
+  .whereNotNull('L.status')
+  .then(rows => rows.map(r => new CardHistory(r)));
 
 module.exports = CardHistory;
--- test/api/model/cardHistory.specs.js
+++ test/api/model/cardHistory.specs.js
@@ -1,5 +1,6 @@
 'use strict';
 const expect = require('chai')
+  .use(require('chai-things'))
   .use(require('chai-as-promised'))
   .expect;
 const tbd = require('tbd');
@@ -19,15 +20,14 @@ describe('Expect CardHistory', () => {
 
   describe('.bulkCreate()', () => {
     it('to insert the given CardHistories', done => {
-      let cardHistories = tbd.from({})
+      const cardHistories = tbd.from({})
       .prop('cardNo').use(tbd.utils.range(1, 10000)).done()
       .prop('listId').use(tbd.utils.range(1, 10000)).done()
-      .make(tbd.utils.range(5, 10)())
-      .map(x => new this.CardHistory(x));
+      .make(tbd.utils.range(5, 10)());
 
       expect(
         this.CardHistory
-          .bulkCreate(cardHistories)
+          .bulkCreate(cardHistories.map(x => new this.CardHistory(x)))
           .then(() => this.knex.select().from('card_histories'))
           .then(rows => 
               cardHistories.every(ch => 
@@ -39,7 +39,7 @@ describe('Expect CardHistory', () => {
         .notify(done); 
     });
     it('to not perform partial inserts', done => {
-      let cardHistories = tbd.from({})
+      const cardHistories = tbd.from({})
       .prop('cardNo').use(tbd.utils.range(1, 10000)).done()
       .prop('listId').use(tbd.utils.range(1, 10000)).done()
       .make(5)
@@ -56,7 +56,7 @@ describe('Expect CardHistory', () => {
         .notify(done); 
     });
     it('to not not allow duplicates', done => {
-      let cardHistories = tbd.from({
+      const cardHistories = tbd.from({
         cardNo: 1,
         listId: 1
       })
@@ -66,6 +66,39 @@ describe('Expect CardHistory', () => {
       expect(this.CardHistory.bulkCreate(cardHistories))
         .to.be.rejected
         .notify(done); 
+    });
+  });
+
+  describe('.list()', () => {
+    it('to return CardHistories that have statuses', done => {
+      const cardHistories = tbd.from({
+        card_no: 1,
+      })
+      .prop('list_id').use(tbd.utils.sequential(1)).done()
+      .make(7);
+      const lists = tbd.from({
+        name: 'blah',
+        order: 1
+      })
+      .prop('id').use(tbd.utils.sequential(3)).done()
+      .make(3);
+
+      lists[0].status = 'backlog';
+      lists[1].status = null;
+      lists[2].status = 'qa';
+
+      this.knex.transaction(tx => 
+        Promise.all([
+          Promise.all(cardHistories.map(ch => tx.insert(ch).into('card_histories'))),
+          Promise.all(lists.map(l => tx.insert(l).into('lists')))
+        ]))
+        .then(() => expect(this.CardHistory.list())
+          .to.eventually.have.length(2)
+          .and.to.eventually.all.be.an.instanceOf(this.CardHistory)
+          .and.to.eventually.all.have.property('status')
+          .and.to.eventually.all.have.property('createdAt')
+          .notify(done))
+        .catch(done);
     });
   });
 });
