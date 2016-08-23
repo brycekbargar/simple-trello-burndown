bbbd1cc31fc4f472f52de3a945017a7233328905
bryce <brycekbargar@gmail.com>
2016-08-23 10:10:41 +0000

    Add List Table

---
+++ migrations/20160823084651_createListTable.js
@@ -0,0 +1,21 @@
+const lists = 'lists';
+const id = 'id';
+const name = 'name';
+const order = 'order';
+const status = 'status';
+
+module.exports  = {
+  up: knex => 
+    knex.schema.createTable(lists, table => {
+      table.integer(id).primary();
+      table.string(name, 16384).notNullable();
+      table.integer(order).notNullable();
+      table.enum(status, [
+        'backlog',
+        'dev',
+        'qa',
+        'done'
+      ]).nullable();
+    }),
+  down: knex => knex.schema.dropTable(lists)
+};
